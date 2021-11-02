c36b359c943ad3683b316f9a76bfbf260559bbc1
hampachovsky <hampe1324@gmail.com>
2021-11-02 20:41:53 +0200

    setup all package for start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
+import { Button } from 'antd';
 import React from 'react';
-import './App.css';
+import './App.less';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='btn'>
+      <Button>CLICK HERE</Button>
+    </div>
+  );
 }
 
 export default App;
