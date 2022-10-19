391e9a602f472eaba17278e6f73a9e26010a0bf5
hampachovsky <hampe1324@gmail.com>
2022-10-19 14:37:26 +0300

    add midellware and config

--- server/index.js
+++ server/index.js
@@ -1,3 +1,28 @@
-const s = 'w';
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import { MONGODB_URI, PORT } from './src/config/index.js';
+import tokenExtractor from './src/middleware/tokenExtractor.js';
+import unknownEndpoint from './src/middleware/unknowEndpoint.js';
 
-console.log('start');
+// basic configuration
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(tokenExtractor);
+
+// if routes not found
+app.use(unknownEndpoint);
+
+async function startApp() {
+    try {
+        await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}!`);
+        });
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+startApp();
